537aa3c1883c4a0105243cd9078c29b051d5e353
Adebola Soyemi <oluyemisisoyemi@gmail.com>
2021-01-14 14:48:49 +0100

    Enabled form verification and submit button action

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,38 @@
-const btn = document.querySelector("button")
+const btn = document.querySelector("button");
+const errorBanner = document.querySelector('#error-banner');
+const defaultRadio = document.getElementById("budget").checked = true;
 
 btn.addEventListener("click", (e) => {
-  window.location.href = "thankyou.html"
+  e.preventDefault()
+  const isFormValid = validate()
+
+  if(isFormValid){
+    window.location.replace("success.html")
+}
+  showBanner(isFormValid)
 })
+
+const requiredElementIds = ["firstName", "lastName", "email", "company", "message", "budget", "referral"]
+
+const validate = () => {
+  let isValid = true
+  requiredElementIds.forEach(id => {
+    let element = document.querySelector(`#${id}`)
+    let isValueEntered = requiredValidator(element)
+    element.classList.toggle("control-error", !isValueEntered)
+    !isValueEntered ? isValid = false : null
+  })
+  return isValid;
+}
+
+const requiredValidator = (element) => {
+  return element.value.trim() ? true : false;
+}
+
+const showBanner = (isValid) => {
+    errorBanner.classList.toggle('show', !isValid)
+  }
+  
+  errorBanner.addEventListener("click", (e)=>{
+    errorBanner.classList.remove("show")
+  })
